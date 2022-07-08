cc6cbf0e3f90c39e32f54d19fc96e1cb8227c10e
Egor Geberleyn <egeberleyn@gmail.com>
2022-07-08 23:37:22 +0300

    Добавьте файлы проекта.

---
+++ HeatmapTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var minMonth = 1;
+            var minDay = 2;           
+            
+            var days = new string[30];
+            for (var x = 0; x < days.Length; x++)          
+                days[x] = (x + minDay).ToString();
+                 
+ 
+            var months = new string[12];
+            for (var y = 0; y < months.Length; y++)
+                months[y] = (y + minMonth).ToString();
+
+            var heat = new double[30, 12];           
+                
+            foreach (var name in names)
+                if (name.BirthDate.Day != 1)
+                    heat[name.BirthDate.Day - minDay, name.BirthDate.Month - minMonth]++;
+
+            return new HeatmapData("Карта интенсивности", heat, days, months);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            Console.WriteLine("Статистика рождаемости по дням");
+            var minDay = 1;                     
+            var days = new string[31];
+            for (var y = 0; y < days.Length; y++)
+                days[y] = (y + minDay).ToString();
+            var birthsCounts = new double[31];
+            foreach (var a in names)
+            {                
+                if (a.Name == name && a.BirthDate.Day != 1)
+                    birthsCounts[a.BirthDate.Day - minDay]++;
+            }
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                days, birthsCounts);
+        }
+    }
+}
